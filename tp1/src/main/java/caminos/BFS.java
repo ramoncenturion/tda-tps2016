a009fd617f5aba7a91d2af2ccdc8bda2e1b487bb
Nico <nicodebin@gmail.com>
2016-10-07 20:15:27 -0300

    mejoras en el BFS

--- tp1/src/main/java/caminos/BFS.java
+++ tp1/src/main/java/caminos/BFS.java
@@ -8,32 +8,25 @@ public class BFS extends Caminos {
 	private double dist[];  // Inicializar a +inf.
 	private Arista edge[];
 	
-	public BFS(Digraph g, int origin, int destino) {
-	    super(g, origin);
-	    // código del algoritmo, rellena "dist" y "edge".
-
-	    //TODO: esto esta bien?
-	    Graph grafo = new Graph(destino);
+	public BFS(Digraph grafo, int origin, int destino) {
+	    super(grafo, origin);
 	    
-	    for (int i = origin; i < destino; i++) {
+	    for (int i = 0; i < grafo.v(); i++) {
 	    	dist[i] = Double.POSITIVE_INFINITY;
-	    	Arista a = new Arista(i, i+1, 0);
-	    	edge[i] = a;
-	    	grafo.addEdge(a);
 	    }
 	    Queue<Integer> q = new LinkedList<Integer>();
 	    dist[origin] = 0;
-	    q.add(origin);
+	    vecinos.add(origin);
 
-        while (!q.isEmpty()) {
-            int v = q.remove();
-            
-            for (int w : g.adj(grafo, v)) {
+        while (!vecinos.isEmpty()) {
+            int v = vecinos.remove();
+
+            for (int w : g.adj(v)) {
                 if (!visitado(w)) {
                 	//TODO: Esto esta bien?
-                	edge[w] = edge[v];
+                	edge[w] = new Arista(v, w, 0);
                 	dist[w] = dist[v] + 1;
-                	q.add(w);
+                	vecinos.add(w);
                 }
             }
         }
@@ -42,15 +35,8 @@ public class BFS extends Caminos {
 	public double distancia(int v) { 
 		return dist[v];
 	}
-	
-	//TODO: para que devuelve una arista? que hace?
+
 	protected Arista edge_to(int v) { 
 		return edge[v]; 
 	}
-
 }
-
-//                    edgeTo[w] = v;
-//                    distTo[w] = distTo[v] + 1;
-//                    marked[w] = true;
-//                    q.enqueue(w);
