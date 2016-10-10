89e802e1956560dd847a5ba8d7aabded4ede1cb2
Gonzalo Adrián Vallerino <gonzalo.vallerino@softtek.com>
2016-10-10 00:21:53 -0300

    Arreglo de bugs. Mejora de codigo. Mas tests de Dijkstra

--- tp1/src/main/java/caminos/Caminos.java
+++ tp1/src/main/java/caminos/Caminos.java
@@ -19,6 +19,22 @@ public abstract class Caminos {
 	
 	protected abstract Arista edge_to(int v);
 	
+	private Arista getAristaByDestino(int v) {
+		Arista arista = null;
+		
+		if (edge.length > 0) {
+			
+			for (int i = 0; i < edge.length; i++) {
+				if (edge[i] != null && edge[i].getDst() == v) {
+					arista = edge[i];
+					break;
+				}
+			}
+		}
+		
+		return arista;
+	}
+	
 	public boolean visitado(int v) {
 		return distancia(v) < Double.POSITIVE_INFINITY;
 	}
@@ -26,27 +42,44 @@ public abstract class Caminos {
 	public List<Arista> camino(int v) {
 		
 		List<Arista> camino = new ArrayList<Arista>();
-		int ultimoSrc = -1;
+		Boolean caminoEncontrado = false;
+		int ultimoSrc = v;
 		
-		for (int i = edge.length - 1; i > 0; i--) {
-			Arista arista = edge[i];
-			
-			if (arista != null) {
-
-				if (arista.getDst() == v || ultimoSrc == arista.getDst()) {
-
-					camino.add(arista);
-					ultimoSrc = arista.getSrc();
-					if (arista.getSrc() == src) {
-						break;
-					}
-				}
-			}
+		while (!caminoEncontrado) {
+			Arista arista = getAristaByDestino(ultimoSrc);
+			camino.add(arista);
+			ultimoSrc = arista.getSrc();
+			caminoEncontrado = ultimoSrc == this.src;
 		}
 		
 		Collections.reverse(camino);
 		return camino;
 	}
+	
+//	public List<Arista> camino(int v) {
+//		
+//		List<Arista> camino = new ArrayList<Arista>();
+//		int ultimoSrc = -1;
+//		
+//		for (int i = edge.length - 1; i > 0; i--) {
+//			Arista arista = edge[i];
+//			
+//			if (arista != null) {
+//				
+//				if (arista.getDst() == v || ultimoSrc == arista.getDst()) {
+//					
+//					camino.add(arista);
+//					ultimoSrc = arista.getSrc();
+//					if (arista.getSrc() == src) {
+//						break;
+//					}
+//				}
+//			}
+//		}
+//		
+//		Collections.reverse(camino);
+//		return camino;
+//	}
 //	
 //	public List<Arista> camino(int v) {
 //		
--- tp1/src/test/java/caminos/TestDijksta.java
+++ tp1/src/test/java/caminos/TestDijksta.java
@@ -3,39 +3,101 @@ package caminos;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestDijksta {
 	private Digraph grafo;
-	private int origin;
+	private int origen;
 	private int destino;
 	private Dijkstra dijkstra;
+	private List<Arista> camino;
 	
-	@Before
-	public void setup() {
-		inicializarValores();
+	private void inicializarGrafo(int cantidadVertices) {
+		grafo = new Digraph(cantidadVertices);
 	}
 	
-	private void inicializarValores() {
-		grafo = new Digraph(4);
-		origin = 0;
-		destino = 3;
+	private void inicializarOrigenDestino(int i, int j) {
+		origen = i;
+		destino = j;
+	}
+	
+	private void encontrarCamino() {
+		dijkstra = new Dijkstra(grafo, origen, destino);
+		camino = dijkstra.camino(destino);
 	}
 	
 	@Test
-	public void testDeberiaEncontrarUnCamino() {
+	public void testDijkstraDeberiaEncontrarUnCamino() {
+		
+		inicializarGrafo(4);
+		inicializarOrigenDestino(0, 3);
+		
 		grafo.add_edge(0, 1, 2);
 		grafo.add_edge(1, 2, 1);
 		grafo.add_edge(2, 3, 3);
 		grafo.add_edge(1, 3, 5);
 		
-		dijkstra = new Dijkstra(grafo, origin, destino);
-		
-	
-		List<Arista> camino = dijkstra.camino(destino);
+		encontrarCamino();
 		
 		Assert.assertTrue(camino != null && camino.size() > 0);
-		//Assert.assertTrue(dijkstra.dist != null && dijkstra.dist.length > 0);
+	}
+	
+	/**
+	 * 2 posibles caminos:
+	 * el minimo tiene longitud 6.
+	 * el otro tiene longitud 7.
+	 */
+	@Test
+	public void testDijkstraDeberiaEncontrarUnCaminoMinimo() {
+
+		double caminoMinimo = 6;
+		inicializarGrafo(4);
+		inicializarOrigenDestino(0, 3);
+		
+		grafo.add_edge(0, 1, 2);
+		grafo.add_edge(1, 2, 1);
+		grafo.add_edge(2, 3, 3);
+		grafo.add_edge(1, 3, 5);
+		
+		encontrarCamino();
+		
+		double sumaDePesos = 0;
+		for (Arista arista : camino) {
+			sumaDePesos += arista.getWeight();
+		}
+		
+		Assert.assertTrue(sumaDePesos == caminoMinimo);
+	}
+	
+	/**
+	 * Se prueba Dijkstra en el grafo de wikipedia:
+	 * https://es.wikipedia.org/wiki/Algoritmo_de_Dijkstra
+	 * Camino Minimo: 20
+	 */
+	@Test
+	public void testDijkstraDeberiaEncontrarUnCaminoMinimoEnUnGrafoComplicado() {
+		
+		double caminoMinimo = 20;
+		inicializarGrafo(6);
+		inicializarOrigenDestino(0, 4);
+		
+		grafo.add_edge(0, 1, 7);
+		grafo.add_edge(0, 2, 9);
+		grafo.add_edge(0, 5, 14);
+		grafo.add_edge(1, 2, 10);
+		grafo.add_edge(1, 3, 15);
+		grafo.add_edge(2, 5, 2);
+		grafo.add_edge(2, 3, 11);
+		grafo.add_edge(5, 4, 9);
+		grafo.add_edge(3, 4, 6);
+		
+		encontrarCamino();
+		
+		double sumaDePesos = 0;
+		for (Arista arista : camino) {
+			sumaDePesos += arista.getWeight();
+		}
+		
+		Assert.assertTrue(sumaDePesos == caminoMinimo);
 	}
 }
