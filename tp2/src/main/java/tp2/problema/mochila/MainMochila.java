022820a972ff79b9755bd4f0b0257dcb29319ac8
Nico <nicodebin@gmail.com>
2016-11-06 17:16:17 -0300

    Version inicial funcionando con ejemplo de prueba del problema de la mochila

--- tp2/src/main/java/tp2/problema/mochila/MainMochila.java
+++ tp2/src/main/java/tp2/problema/mochila/MainMochila.java
@@ -1,5 +1,18 @@
 package tp2.problema.mochila;
 
-public class MainMochila {
+import java.util.List; 
 
+public class MainMochila {
+    public static void main(String[] args) {
+    	Mochila mochila = new Mochila(5);
+    	
+    	mochila.agregarItem(2, 3);
+    	mochila.agregarItem(3, 4);
+    	mochila.agregarItem(4, 5);
+    	mochila.agregarItem(5, 6);
+  
+        System.out.println(mochila.toString()); 
+        List<Item> solucion = mochila.resolver(); 
+        System.out.println("Solución: " + solucion.toString()); 
+    } 
 }
