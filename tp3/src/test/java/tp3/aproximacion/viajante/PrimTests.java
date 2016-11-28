2ba05bd43188b5470b0e9b9ea886203ee25b88a6
Gonzalo Vallerino <Gonzalo Vallerino>
2016-11-28 20:04:40 -0300

    Se commitea otro ejemplo. Sigue andando mal

--- tp3/src/test/java/tp3/aproximacion/viajante/PrimTests.java
+++ tp3/src/test/java/tp3/aproximacion/viajante/PrimTests.java
@@ -40,5 +40,33 @@ public class PrimTests {
 		
 		Prim prim = new Prim(grafo, 0);
 	}
+	
+	/**
+	 * Test del grafo de wikipedia
+	 * https://es.wikipedia.org/wiki/Algoritmo_de_Prim
+	 */
+	@Test
+	public void test2() {
+		inicializarGrafo();
+		
+		grafo.addArista(0, 1, 7);
+		grafo.addArista(0, 3, 5);
+		
+		grafo.addArista(1, 2, 8);
+		grafo.addArista(1, 3, 9);
+		grafo.addArista(1, 4, 7);
+		
+		grafo.addArista(2, 4, 5);
+		
+		grafo.addArista(3, 4, 15);
+		grafo.addArista(3, 5, 6);
+		
+		grafo.addArista(4, 5, 8);
+		grafo.addArista(4, 6, 9);
+		
+		grafo.addArista(5, 6, 11);
+		
+		Prim prim = new Prim(grafo, 3);
+	}
 
 }
