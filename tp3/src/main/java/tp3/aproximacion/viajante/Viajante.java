15559116b459a8be8f2f8d87b3341b5123e3f344
Gonzalo Vallerino <Gonzalo Vallerino>
2016-11-30 22:18:32 -0300

    Agrego viajante sin andar

--- tp3/src/main/java/tp3/aproximacion/viajante/Viajante.java
+++ tp3/src/main/java/tp3/aproximacion/viajante/Viajante.java
@@ -1,5 +1,14 @@
 package tp3.aproximacion.viajante;
 
+import tp3.aproximacion.viajante.Graph;
+
 public class Viajante {
 
+	private Prim arbolMinimo = null;
+	
+	
+	public Viajante (Graph grafo, int raiz) {
+		
+		arbolMinimo = new Prim(grafo, raiz);
+	}
 }
