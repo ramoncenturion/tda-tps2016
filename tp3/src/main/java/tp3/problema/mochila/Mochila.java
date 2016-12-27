448a7919a5b0bc3be3aebbdf6dc4382ff8b54bcf
nicolasd <nicolas.debin@rga.com>
2016-12-27 16:34:44 -0300

    Cambio en la ecuacion de recurencia del problema de la mochila (problema de aproximacion)

--- tp3/src/main/java/tp3/problema/mochila/Mochila.java
+++ tp3/src/main/java/tp3/problema/mochila/Mochila.java
@@ -8,90 +8,95 @@ import java.util.List;
 public class Mochila {
     private List<Item> listaItems; 
     private int pesoMaximo;
-    private int[][] P; // Matriz de soluciones
+    private int[][] M; // Matriz de soluciones
+    private int V; // Suma de valores
      
     public Mochila(int pesoMaximo){ 
         this.listaItems = new LinkedList<Item>(); 
         this.pesoMaximo = pesoMaximo; 
+        this.V = 0;
     } 
     
     public void agregarItem(int peso, int valor) {
-    	if (valor <= this.pesoMaximo) {
-    		this.listaItems.add(new Item(peso,valor));
-    	}
+        if (peso <= this.pesoMaximo) {
+            this.listaItems.add(new Item(peso,valor));
+            this.V += valor;
+        }
     }
     
     // Approximation algorithm
     public void overrideValues(double epsilon) {
-    	// Buscar el maximo valor
-    	Item maxItemValue = Collections.max(listaItems);
-    	int maxValue = maxItemValue.getValor();
-    	int n = listaItems.size();
-    	
-    	// Source: Vazirani, Dasgupta - Algorithms.
-    	double b = n/(epsilon*maxValue);
-    	
-    	for (Item item : listaItems) {
-    		item.setValor((int)Math.floor(item.getValor()*b));
-    	}
+        // Buscar el maximo valor
+        Item maxItemValue = Collections.max(listaItems);
+        int maxValue = maxItemValue.getValor();
+        int n = listaItems.size();
+        
+        // Source: Vazirani, Dasgupta - Algorithms.
+        double b = n/(epsilon*maxValue);
+        
+        
+        this.V = 0;
+        for (Item item : listaItems) {
+            int valor = (int)Math.floor(item.getValor()*b);
+            item.setValor(valor);
+            this.V += valor;
+        }
     }
     
     public MochilaResult resolver() {
-    	int n = this.listaItems.size(); // Cantidad de elementos 
-    	int W = this.pesoMaximo;
-    	
-    	calcularMatrizdeSoluciones(n, W);
-    	List<Item> itemsSolucion = analizarSolucion(n, W);
-    	
-    	return new MochilaResult(itemsSolucion, P[n][W]);
+        int n = this.listaItems.size(); // Cantidad de elementos 
+        int W = this.pesoMaximo;
+        
+        calcularMatrizdeSoluciones(n);
+        List<Item> itemsSolucion  = new LinkedList<Item>();
+
+        int max = -1;
+        for (int v=0 ; v<=V ; v++) {
+            if (M[n][v] <= this.pesoMaximo) {
+                if (max < M[n][v]) {
+                    max = M[n][v];
+                }
+            }
+        }
+        
+        return new MochilaResult(itemsSolucion, max);
     }
     
-    private void calcularMatrizdeSoluciones(int n, int W) {
-    	P = new int[n+1][W+1]; // Matriz de soluciones
-    	
-    	// Inicializacion de la matriz
-    	for (int w=0 ; w<=W ; w++) {
-    		P[0][w] = 0; 
-    	}
-    	for (int i=0 ; i<=n ; i++) {
-    		P[i][0] = 0; 
-    	}
-    	
-    	for (int i=1 ; i<=n ; i++) {
-    		for (int w=0 ; w<=W ; w++) {
-    			Item item = listaItems.get(i-1);
-    			int wi = item.getPeso();
-    			if (wi <= w) { // el elemento i puede ser parte de la solucion
-    				if (item.getValor() + P[i-1][w-wi] > P[i-1][w]) {
-    					P[i][w] = item.getValor() + P[i-1][w-wi]; 
-    				} else {
-    					P[i][w] = P[i-1][w];
-    				}
-    			} else {
-    				P[i][w] = P[i-1][w]; // wi > w
-    			}
-    		}
-    	}    	
+    private void calcularMatrizdeSoluciones(int n) {
+        M = new int[n+1][V+1]; // Matriz de soluciones
+        
+        // Inicializacion de la matriz
+        for (int v=0 ; v<=V ; v++) {
+            M[0][v] = 0; 
+        }
+        for (int i=0 ; i<=n ; i++) {
+            M[i][0] = 0; 
+        }
+        
+        for (int i=1 ; i<=n ; i++) {
+            int sumV = this.getSumaValores(i);
+
+            for (int v=1 ; v<=sumV ; v++) {
+                Item item = listaItems.get(i-1);
+                int wi = item.getPeso();
+                int vi = item.getValor();
+
+                if (v > this.getSumaValores(i-1)) {
+                    M[i][v] = wi + M[i-1][v];
+                } else {
+                    M[i][v] = Math.min(M[i-1][v], wi + M[i-1][Math.max(0,v-vi)]);
+                }
+            }
+        }       
     }
-    
-    private List<Item> analizarSolucion(int i, int k) {
-    	List<Item> itemsSolucion  = new LinkedList<Item>();
-    	
-    	while (i*k > 0) {
-	    	if (P[i][k] != P[i-1][k]) {
-	    		Item item = listaItems.get(i-1);
-	    		itemsSolucion.add(item);
-	    		i--;
-	    		k -= item.getPeso();
-	    	} else {
-	    		i--;
-	    	}
-    	}
-    	
-    	return itemsSolucion;
+
+    private int getSumaValores(int i) {
+        int sum = 0;
+        for (int j=1 ; j<=i ; j++) {
+            Item item = listaItems.get(j-1);
+            sum += item.getValor();
+        }
+        return sum;
     }
-    
-    public String toString(){ 
-        return "Peso máximo: " + pesoMaximo + "\nLista de items: " + listaItems.toString(); 
-    }
+
 }
